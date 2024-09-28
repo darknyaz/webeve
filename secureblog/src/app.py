aa4ddf7c9678bb5e2c492086ceaea49a2f7d5ee1
darknyaz <darknyaz>
2024-09-28 05:13:09 -0400

    Add users

--- secureblog/src/app.py
+++ secureblog/src/app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 import psycopg2
 import os
 
 app = Flask(__name__)
+app.secret_key = 'BAD_SECRET_KEY'
 POSTGRES_USER = os.environ.get("POSTGRES_USER")
 POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
 POSTGRES_HOST = os.environ.get("POSTGRES_HOST")
@@ -14,25 +15,65 @@ conn = psycopg2.connect(
     host=POSTGRES_HOST,
     port=5432
 )
-#conn.set_session(autocommit=True)
 
 def get_posts():
     with conn.cursor() as cur:
-        cur.execute("SELECT text FROM post;")
-        return [x[0] for x in cur.fetchall()]
+        cur.execute("SELECT text, login FROM post;")
+        return [(x[0], x[1]) for x in cur.fetchall()]
 
 
-def create_post(post):
+def create_post(post, login):
     with conn.cursor() as cur:
-        cur.execute(f"INSERT INTO post(text) VALUES ('{post}');")
+        cur.execute(f"INSERT INTO post(text, login) VALUES ('{post}', '{login}');")
+
+
+def get_user(login):
+    with conn.cursor() as cur:
+        cur.execute(f"SELECT login FROM bloguser WHERE login = '{login}';")
+        return next(iter(cur.fetchall()), [None])[0]
+
+
+def create_user(login):
+    if get_user(login) is not None:
+        return
+    with conn.cursor() as cur:
+        cur.execute(f"INSERT INTO bloguser(login) VALUES ('{login}');")
 
 
 @app.route("/")
 def index():
-    return render_template("index.html", posts=get_posts())
+    return render_template("index.html", posts=get_posts(), login=session.get("login"))
 
 
 @app.route("/posts", methods=["POST"])
 def posts():
-    create_post(request.form["text"])
+    if session.get("login") is not None:
+        create_post(request.form["text"], session["login"])
+    return redirect("/")
+
+
+@app.route("/users", methods=["POST"])
+def users():
+    create_user(request.form["login"])
+    return redirect("/login")
+
+
+@app.route("/register", methods=["GET"])
+def register():
+    return render_template("register.html", login=session.get("login"))
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html", login=session.get("login"))
+    login = request.form["login"]
+    if get_user(login) is not None:
+        session['login'] = login
+    return redirect("/") 
+
+
+@app.route("/logout", methods=["GET"])
+def logout():
+    session.pop("login")
     return redirect("/")
