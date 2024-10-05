1a37330b0965a4a4a09dc1b274ee1ca357a45de9
darknyaz <darknyaz>
2024-10-05 10:33:36 -0400

    Add hosts and SameSite

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # webeve
+
+## Domain naming
+
+Append lines from file `hosts` to your `/etc/hosts`. After it sites is avaliable
+
+| Application | URL |
+|-------------|-------------------------------|
+| secureblog | http://secureblog |
+| evilsite | http://evilsite |
--- secureblog/src/app.py
+++ secureblog/src/app.py
@@ -4,6 +4,7 @@ import os
 
 app = Flask(__name__)
 app.secret_key = 'BAD_SECRET_KEY'
+app.config["SESSION_COOKIE_SAMESITE"] = "Strict"
 POSTGRES_USER = os.environ.get("POSTGRES_USER")
 POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
 POSTGRES_HOST = os.environ.get("POSTGRES_HOST")
